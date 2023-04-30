435e6f338c5b79426c4956764a891cd17350870f
Vinrobot <vinrobot97@gmail.com>
2023-04-30 16:31:59 +0200

    Generate content library from releases json

--- src/commands/generate/index.ts
+++ src/commands/generate/index.ts
@@ -1,8 +1,9 @@
 import { Args, Command, Flags, ux } from '@oclif/core';
+import { readFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 
-import { buildContentLibrary } from '../../lib/vmware';
-import { FileSystemProvider } from '../../lib/vmware/providers';
+import { Release } from '../../lib/ubuntu';
+import { buildContentLibrary, ReleasesProvider } from '../../lib/vmware';
 
 export default class Generate extends Command {
   static description = '';
@@ -15,14 +16,19 @@ export default class Generate extends Command {
 
   static args = {
     libPath: Args.string({ required: true }),
+    releasesPath: Args.string({ required: true }),
   };
 
   async run(): Promise<void> {
     const { args, flags } = await this.parse(Generate);
 
     const libPath = resolve(args.libPath);
+    const releasesPath = resolve(args.releasesPath);
 
-    const provider = new FileSystemProvider(libPath);
+    const content = await readFile(releasesPath, { encoding: 'utf-8' });
+    const releases = JSON.parse(content) as Release[];
+    // TODO: Validate releases with schema
+    const provider = new ReleasesProvider(releases);
 
     ux.action.start('Generating files...');
 
--- src/lib/vmware/providers.ts
+++ src/lib/vmware/providers.ts
@@ -3,6 +3,7 @@ import { createReadStream } from 'node:fs';
 import { readdir, stat } from 'node:fs/promises';
 import { basename, join } from 'node:path';
 
+import { Release } from '../ubuntu';
 import { filterAsync, updateHash } from './helpers';
 import { ContentLibraryProvider, Directory, File } from './types';
 
@@ -60,5 +61,50 @@ class FileSystemFile implements File {
       stream.on('data', (data: any) => hasher.update(data));
       stream.on('end', () => resolve(hasher));
     });
+  }
+}
+
+export class ReleasesProvider implements ContentLibraryProvider {
+  constructor(private releases: Release[]) { }
+
+  async getDirectories(): Promise<Directory[]> {
+    return this.releases.map(r => new ReleaseDirectory(r));
+  }
+}
+
+class ReleaseDirectory implements Directory {
+  readonly name: string;
+
+  constructor(private release: Release) {
+    this.name = `ubuntu-${release.version}-server-cloudimg-${release.arch}-${release.release}`;
+  }
+
+  async getFiles(): Promise<File[]> {
+    const file = this.release.file!;
+    return [new ReleaseFile(this.name + '.ova', file.size, file.etag)];
+  }
+
+  async updateHash(hasher: Hash, files?: File[]): Promise<Hash> {
+    return updateHash(hasher, files ?? await this.getFiles());
+  }
+}
+
+class ReleaseFile implements File {
+  private readonly etag: string;
+
+  constructor(
+    readonly name: string,
+    private readonly size: number,
+    etag?: string | null,
+  ) {
+    this.etag = etag ?? JSON.stringify([name, size]);
+  }
+
+  async getSize(): Promise<number> {
+    return this.size;
+  }
+
+  async updateHash(hasher: Hash): Promise<Hash> {
+    return hasher.update(this.etag);
   }
 }
