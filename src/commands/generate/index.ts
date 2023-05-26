469ea9562e6cfb15b097e40dcc323f73c251caaa
Vinrobot <vinrobot97@gmail.com>
2023-05-27 00:21:40 +0200

    Read from stdin

--- src/commands/generate/index.ts
+++ src/commands/generate/index.ts
@@ -1,9 +1,31 @@
 import { Args, Command, Flags, ux } from '@oclif/core';
-import { readFile } from 'node:fs/promises';
+import { readFile as fsReadFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
+import { createInterface } from 'node:readline';
 
 import { Release } from '../../lib/ubuntu';
 import { buildContentLibrary, ReleasesProvider } from '../../lib/vmware';
+
+function readFromStdin(): Promise<string> {
+  return new Promise(resolve => {
+    const rl = createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false,
+    });
+    const lines: string[] = [];
+    rl.on('line', line => lines.push(line));
+    rl.once('close', () => resolve(lines.join('')));
+  });
+}
+
+function readFile(file: string) {
+  if (file === '-') {
+    return readFromStdin();
+  }
+
+  return fsReadFile(resolve(file), { encoding: 'utf-8' });
+}
 
 export default class Generate extends Command {
   static description = '';
@@ -23,9 +45,8 @@ export default class Generate extends Command {
     const { args, flags } = await this.parse(Generate);
 
     const libPath = resolve(args.libPath);
-    const releasesPath = resolve(args.releasesPath);
 
-    const content = await readFile(releasesPath, { encoding: 'utf-8' });
+    const content = await readFile(args.releasesPath);
     const releases = JSON.parse(content) as Release[];
     // TODO: Validate releases with schema
     const provider = new ReleasesProvider(releases);
